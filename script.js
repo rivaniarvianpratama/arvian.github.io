3c225280b0ad4a4d3cb98b4f612e784bc18b4840
rivaniarvianpratama <rivani1700016077@webmail.uad.ac.id>
2019-10-31 17:17:09 +0700

    update uts

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all ([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('/mulon/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/mulon/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/mulon/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/mulon/models'),
 ]).then(startVideo)
 
 function startVideo() {
